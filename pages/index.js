d6bab542baa52f0419dca518b82314cac4d1fe59
Chorom Ham <chorom-ham@gmail.com>
2019-11-03 17:18:48 +0900

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    hi
+  </div>
+);
+
+export default Home;
