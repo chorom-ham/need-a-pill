7e28d8d9f3266cccccc2be8c678860e170ea406e
Chorom Ham <chorom-ham@gmail.com>
2019-12-24 04:27:06 +0900

    팝업창 접기 구현

--- src/components/organisms/popup.js
+++ src/components/organisms/popup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Popup from "reactjs-popup";
 import styled from "styled-components";
 import Logo from "../atoms/icon/logo";
@@ -6,149 +6,175 @@ import Ministry from "../atoms/icon/ministry";
 import Text from "../atoms/text";
 import CloseBtn from "../atoms/Button/popup";
 
-export default () => (
-  <Popup
-    modal={true}
-    lockScroll={true}
-    defaultOpen={true}
-    closeOnDocumentClick={false}
-    position="right center"
-    contentStyle={contentStyle}
-  >
-    {close => (
-      <Wrapper>
-        <Logo
-          style={{
-            width: "14.5rem",
-            height: "2.85rem",
-            margin: "2.76rem 0 1.2rem 0"
-          }}
-        ></Logo>
-        <Text
-          level={4}
-          color="#3446d4"
-          spacing="-0.048rem"
-          align="center"
-          width="22rem"
-        >
-          Service for foreigners residing in Korea who are in need of medicinal
-          information.
-        </Text>
-        <Line></Line>
-        <Ministry
-          style={{ width: "3.6rem", height: "3.4rem", marginBottom: "0.8rem" }}
-        ></Ministry>
-        <Text
-          level={2}
-          spacing="-0.032rem"
-          color="#b9b9ba"
-          align="center"
-          width="22rem"
-        >
-          Needapill makes it a high-priority principle to provide only
-          information approved by Ministry of Health & Welfare of Korea.
-        </Text>
-        <Line></Line>
-        <InfoWrapper>
+export default function Modal() {
+  const [showMore, setShowMore] = useState(false);
+
+  return (
+    <Popup
+      modal={true}
+      lockScroll={true}
+      defaultOpen={true}
+      closeOnDocumentClick={false}
+      position="right center"
+      contentStyle={contentStyle}
+    >
+      {close => (
+        <Wrapper>
+          <Logo
+            style={{
+              width: "14.5rem",
+              height: "2.85rem",
+              margin: "2.76rem 0 1.2rem 0"
+            }}
+          ></Logo>
           <Text
             level={4}
-            weight="bold"
+            color="#3446d4"
             spacing="-0.048rem"
-            color="#b9b9ba"
-            align="left"
-            width="28rem"
-          >
-            For Your Information:
-          </Text>
-          <br />
-          <Text
-            width="28rem"
-            level={3.5}
-            spacing="-0.044rem"
-            align="left"
-            color="#b9b9ba"
-          >
-            1. Every information provided by this service can be used for
-            informational reference purposes only – for exact identification of
-            symptoms, visit a doctor or pharmacist.
-          </Text>
-          <br />
-          <Text
-            width="28rem"
-            level={3.5}
-            spacing="-0.044rem"
-            align="left"
-            color="#b9b9ba"
-          >
-            2. Every information provided by this service is not a
-            representation of a medical opinion; persons and entrepreneurs who
-            provided the search results are not obligated to legal
-            responsibilities.
-          </Text>
-          <br />
-          <Text
-            width="28rem"
-            level={3.5}
-            spacing="-0.044rem"
-            align="left"
-            color="#b9b9ba"
-          >
-            3. Every information provided by this service can potentially carry
-            errors due to different medical opinions, mistyped text, and
-            translation process; it does not encompass every medical opinion
-            that may vary by different medical personnel.
-          </Text>
-          <br />
-          <Text
-            width="28rem"
-            level={3.5}
-            spacing="-0.044rem"
-            align="left"
-            color="#b9b9ba"
-          >
-            4. Personnel associated with this service do not have obligations to
-            compensate for any damages the user may have experienced due to
-            change in content of this service that does not come at purpose or
-            by negligence of the personnel.
-          </Text>
-        </InfoWrapper>
-        <CloseBtn
-          color="#3446d4"
-          onClick={() => {
-            close();
-          }}
-        >
-          <Text
-            level={5}
-            weight="bold"
-            spacing="-0.056rem"
             align="center"
-            color="#fff"
-            deco="underline"
+            width="22rem"
           >
-            Okay. I agree
+            Service for foreigners residing in Korea who are in need of
+            medicinal information.
           </Text>
-        </CloseBtn>
-        <CloseBtn
-          color="#ececf0"
-          onClick={() => {
-            close();
-          }}
-        >
+          <Line></Line>
+          <Ministry
+            style={{
+              width: "3.6rem",
+              height: "3.4rem",
+              marginBottom: "0.8rem"
+            }}
+          ></Ministry>
           <Text
-            level={5}
-            weight="bold"
-            spacing="-0.056rem"
+            level={2}
+            spacing="-0.032rem"
+            color="#b9b9ba"
             align="center"
-            color="#333"
+            width="22rem"
           >
-            Remind me later
+            Needapill makes it a high-priority principle to provide only
+            information approved by Ministry of Health & Welfare of Korea.
           </Text>
-        </CloseBtn>
-      </Wrapper>
-    )}
-  </Popup>
-);
+          <Line></Line>
+          <InfoWrapper>
+            <Text
+              level={4}
+              weight="bold"
+              spacing="-0.048rem"
+              color="#b9b9ba"
+              align="left"
+              width="28rem"
+            >
+              For Your Information:
+            </Text>
+            <br />
+            <Text
+              width="28rem"
+              level={3.5}
+              spacing="-0.044rem"
+              align="left"
+              color="#b9b9ba"
+            >
+              1. Every information provided by this service can be used for
+              informational reference purposes only – for exact identification
+              of symptoms, visit a doctor or pharmacist.
+            </Text>
+            {!showMore && (
+              <>
+                <MoreBtn onClick={() => setShowMore(true)}>
+                  <Text
+                    level={2}
+                    spacing="-0.032rem"
+                    deco="underline"
+                    color="#3446d4"
+                  >
+                    Read More
+                  </Text>
+                </MoreBtn>
+              </>
+            )}
+            {showMore && (
+              <>
+                <br />
+                <Text
+                  width="28rem"
+                  level={3.5}
+                  spacing="-0.044rem"
+                  align="left"
+                  color="#b9b9ba"
+                >
+                  2. Every information provided by this service is not a
+                  representation of a medical opinion; persons and entrepreneurs
+                  who provided the search results are not obligated to legal
+                  responsibilities.
+                </Text>
+                <br />
+                <Text
+                  width="28rem"
+                  level={3.5}
+                  spacing="-0.044rem"
+                  align="left"
+                  color="#b9b9ba"
+                >
+                  3. Every information provided by this service can potentially
+                  carry errors due to different medical opinions, mistyped text,
+                  and translation process; it does not encompass every medical
+                  opinion that may vary by different medical personnel.
+                </Text>
+                <br />
+                <Text
+                  width="28rem"
+                  level={3.5}
+                  spacing="-0.044rem"
+                  align="left"
+                  color="#b9b9ba"
+                >
+                  4. Personnel associated with this service do not have
+                  obligations to compensate for any damages the user may have
+                  experienced due to change in content of this service that does
+                  not come at purpose or by negligence of the personnel.
+                </Text>
+              </>
+            )}
+          </InfoWrapper>
+          <CloseBtn
+            color="#3446d4"
+            onClick={() => {
+              close();
+            }}
+          >
+            <Text
+              level={5}
+              weight="bold"
+              spacing="-0.056rem"
+              align="center"
+              color="#fff"
+              deco="underline"
+            >
+              Okay. I agree
+            </Text>
+          </CloseBtn>
+          <CloseBtn
+            color="#ececf0"
+            onClick={() => {
+              close();
+            }}
+          >
+            <Text
+              level={5}
+              weight="bold"
+              spacing="-0.056rem"
+              align="center"
+              color="#333"
+            >
+              Remind me later
+            </Text>
+          </CloseBtn>
+        </Wrapper>
+      )}
+    </Popup>
+  );
+}
 
 const contentStyle = {
   overflowY: "scroll",
@@ -185,4 +211,10 @@ const InfoWrapper = styled.div`
   justify-content: center;
   margin-top: 0.4rem;
   margin-bottom: 2rem;
+`;
+
+const MoreBtn = styled.div`
+  border: none;
+  background-color: transparent;
+  margin-top: 0.8rem;
 `;
