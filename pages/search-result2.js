ae2085830e33f2cbac64a50d7df50a29fb3b3853
Chorom Ham <chorom-ham@gmail.com>
2019-12-25 21:27:12 +0900

    헤더와 텍스트박스 겹치는 오류 수정

--- pages/search-result2.js
+++ pages/search-result2.js
@@ -13,7 +13,6 @@ export default function SearchResult() {
         <title>Need a Pill</title>
       </Head>
       <MainHeader></MainHeader>
-
       <SearchResults></SearchResults>
     </Wrapper>
   );
@@ -21,4 +20,6 @@ export default function SearchResult() {
 
 const Wrapper = styled.div`
   background-color: #fff;
+  display: flex;
+  flex-direction: column;
 `;
