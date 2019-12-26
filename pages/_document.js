695cbde0d93a17ec690470a38703465534e0d3e0
Chorom Ham <chorom-ham@gmail.com>
2019-12-26 18:25:05 +0900

    메타 태그 설저

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,9 @@
 import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
+const siteDescription =
+  "Needapill is a service for foreigners residing in Korea who are in need of medicinal information.";
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -33,6 +36,13 @@ export default class MyDocument extends Document {
           <meta
             name="viewport"
             content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1"
+          />
+          <meta name="description" content={siteDescription} />
+          <meta property="og:title" content={"Need a Pill"} />
+          <meta property="og:description" content={siteDescription} />
+          <meta
+            property="og:image"
+            content="https://lithub.com/wp-content/uploads/2019/03/pills.jpg"
           />
           <script
             data-ad-client="ca-pub-5202070596232358"
