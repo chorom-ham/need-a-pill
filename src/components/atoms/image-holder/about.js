987e90c0767c1872241737d0725fa39f7d39c331
chorom.m.ham@gmail.com <chorom.m.ham@gmail.com>
2020-02-24 20:30:08 +0900

    fix menu header style & complete 'about' page

---
+++ src/components/atoms/image-holder/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+export default styled.div`
+  width: 100%
+  height: 20rem;
+  object-fit: contain;
+  padding-bottom: 0.8rem;
+  background-color: #ececf0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- src/components/organisms/Header/menuHeader.js
+++ src/components/organisms/Header/menuHeader.js
@@ -5,25 +5,26 @@ import BackButton from "../../molecules/Button/back";
 
 function mainHeader(props) {
   return (
-    <MainHeaderWrapper>
+    <MenuHeaderWrapper>
       <BackButtonWrapper>
         <BackButton></BackButton>
       </BackButtonWrapper>
       <Text level={6} weight="bold" spacing="-0.064rem" color="#3446d4">
         {props.headerText}
       </Text>
-    </MainHeaderWrapper>
+    </MenuHeaderWrapper>
   );
 }
 
-const MainHeaderWrapper = styled.header`
+const MenuHeaderWrapper = styled.header`
   width: 100%;
   background-color: #ffffff;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  margin: 5.6rem 0 2.91rem 0;
+  padding: 4.6rem 0 1.6rem 0;
+  border-bottom: 0.4rem solid #ececf0;
 `;
 
 const BackButtonWrapper = styled.div`
