e5ee895ec7dcb3c130893515ce29376b7fcc350a
chorom.m.ham@gmail.com <chorom.m.ham@gmail.com>
2020-03-14 18:56:23 +0900

    add notice page

--- pages/notice.js
+++ pages/notice.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Header from "../src/components/organisms/Header/menuHeader";
 import Posts from "../src/components/templates/post-list";
 
-export default function About() {
+export default function Notice() {
   return (
     <>
       <Header headerText="Notice"></Header>
